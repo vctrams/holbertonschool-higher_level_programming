79ab6bc2fd577303ed9d298b14aa5aff8b46a94c
vctrams <vctrams@vctrams@gmail.com>
2024-01-31 12:26:31 -0800

    Replace in a copy

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -2,7 +2,8 @@
 def new_in_list(my_list, idx, element):
     i = my_list[:]
     if idx < 0:
-        return(i)
+        return i
     if idx > len(my_list) - 1:
-        return(i)
+        return i
+    i[idx] = element
     return i
