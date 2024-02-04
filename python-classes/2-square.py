74513626645a287974906e99197a0e2ce93322c4
vctrams <vctrams@vctrams@gmail.com>
2024-02-04 15:45:21 -0800

    Size validation

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
-"""Defines a Square"""
+"""Defines a Square."""
 
 class Square:
-    """Represent a square"""
+    """Represent a square."""
 
     def __init__(self, size=0):
         """Initialize a new square
